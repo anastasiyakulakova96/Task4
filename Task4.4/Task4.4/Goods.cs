3446079ef905809c3718218e29d4afd8679b4670
AnastasiyaKulakova <kylalova.n@mail.ru>
2016-07-04 15:30:50 +0300

    add project

---
+++ Task4.4/Task4.4/Goods.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4._4
+{
+    class Goods
+    {
+        public List<Product> goods;
+
+        public List<Product> ListOfGoods()
+        {
+            goods = new List<Product>();
+            goods.Add(new Product(1, "Phone", 100, 0));
+            goods.Add(new Product(2, "Notebook", 200, 0));
+            goods.Add(new Product(3, "TV", 150, 0));
+
+            return goods;
+        }
+
+        public void ShowGoodsAndPrice()
+        {
+            for (int i = 0; i < goods.Count; i++)
+            {
+                Console.WriteLine("ID:" + goods[i].getId() + " name:" + goods[i].getName() + " price:" + goods[i].getPrice());
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
---
+++ Task4.4/Task4.4/Product.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4._4
+{
+    class Product
+    {
+        private int id;
+        private String name;
+        private int price;
+        private int count;
+
+        public Product(int id,String name, int price)
+        {
+            this.id = id;
+            this.name = name;
+            this.price = price;
+        }
+        public Product(int id, String name, int price, int count)
+        {
+            this.id = id;
+            this.name = name;
+            this.price = price;
+            this.count = count;
+        }
+
+
+        public void setCount(int count)
+        {
+            this.count = count;
+        }
+
+        public int getPrice()
+        {
+            return price;
+        }
+
+        public String getName()
+        {
+            return name;
+        }
+
+        public int getId()
+        {
+            return id;
+        }
+
+        public int getCount()
+        {
+            return count;
+        }
+    }
+}
